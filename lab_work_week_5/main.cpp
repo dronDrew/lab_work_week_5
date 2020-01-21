d83e89278f0eaef058fec71b90be8611a072c1d1
Андрій Дудчак <andrew.melauda@gmail.com>
2020-01-21 21:22:25 +0200

    v.0.0.1

--- lab_work_week_5/main.cpp
+++ lab_work_week_5/main.cpp
@@ -1,8 +1,20 @@
 #include "dependies.h"
 #include "list_template.h"
-
+#include "line_to_direction_template.h"
 using namespace std;
 
 int main() {
+	list_two_direcktion<int> a;
+	a.Add_to_tail(15);
+	//a.Show();
+	a.Add_to_tail(18);
+	a.Add_to_tail(18);
+	a.Add_to_position(3, 18);
+	//a.Delete_from_position(3);
+	a.Show();
+	cout <<"___________________________________________"<< endl;
+	a.Direction_swap();
+	cout << "___________________________________________" << endl;
+	a.Show();
 	return 0;
 }
